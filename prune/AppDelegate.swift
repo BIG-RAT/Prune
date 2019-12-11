de33838d844c39c95878a474bee846899dfd13d0
leslie <leslie@ccs-indy.com>
2019-12-11 09:49:39 -0500

    Initial Commit

---
+++ prune/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  prune
+//
+//  Created by Leslie Helou on 12/11/19.
+//  Copyright © 2019 Leslie Helou. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ prune/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  prune
+//
+//  Created by Leslie Helou on 12/11/19.
+//  Copyright © 2019 Leslie Helou. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
