d7f8fc95ecefbd13c7de82d8d982a998b51a3abd
leslie <leslie@ccs-indy.com>
2019-12-15 22:03:05 -0500

    removals working

---
+++ prune/Xml.swift
@@ -0,0 +1,70 @@
+//
+//  Xml.swift
+//  Prune
+//
+//  Created by Leslie Helou on 12/15/19.
+//  Copyright © 2019 Leslie Helou. All rights reserved.
+//
+
+import Cocoa
+
+class Xml: NSURL, URLSessionDelegate {
+    func action(action: String, theServer: String, base64Creds: String, theEndpoint: String, completion: @escaping (_ result: [String:AnyObject]) -> Void) {
+
+        let getRecordQ = DispatchQueue(label: "com.jamf.getRecordQ", qos: DispatchQoS.background)
+    
+        URLCache.shared.removeAllCachedResponses()
+        var existingDestUrl = ""
+        
+        existingDestUrl = "\(theServer)/JSSResource/\(theEndpoint)"
+        existingDestUrl = existingDestUrl.replacingOccurrences(of: "//JSSResource", with: "/JSSResource")
+        
+//        if LogLevel.debug { WriteToLog().message(stringOfText: "[Json.getRecord] Looking up: \(existingDestUrl)\n") }
+//        print("existing endpoints URL: \(existingDestUrl)")
+        let destEncodedURL = NSURL(string: existingDestUrl)
+        let jsonRequest    = NSMutableURLRequest(url: destEncodedURL! as URL)
+        
+        let semaphore = DispatchSemaphore(value: 1)
+        getRecordQ.async {
+            
+            jsonRequest.httpMethod = "\(action)"
+            let destConf = URLSessionConfiguration.default
+            destConf.httpAdditionalHeaders = ["Authorization" : "Basic \(base64Creds)", "Content-Type" : "text/xml", "Accept" : "text/xml"]
+            let destSession = Foundation.URLSession(configuration: destConf, delegate: self, delegateQueue: OperationQueue.main)
+            let task = destSession.dataTask(with: jsonRequest as URLRequest, completionHandler: {
+                (data, response, error) -> Void in
+                if let httpResponse = response as? HTTPURLResponse {
+//                    print("[Json.getRecord] httpResponse: \(String(describing: httpResponse))")
+                    if httpResponse.statusCode >= 200 && httpResponse.statusCode <= 299 {
+                        do {
+                            let json = try? JSONSerialization.jsonObject(with: data!, options: .allowFragments)
+                            if let endpointJSON = json as? [String:AnyObject] {
+//                                if LogLevel.debug { WriteToLog().message(stringOfText: "[Json.getRecord] \(endpointJSON)\n") }
+                                completion(endpointJSON)
+                            } else {
+//                                WriteToLog().message(stringOfText: "[Json.getRecord] error parsing JSON for \(existingDestUrl)\n")
+                                completion([:])
+                            }
+                        }
+                    } else {
+//                        WriteToLog().message(stringOfText: "[Json.getRecord] error HTTP Status Code: \(httpResponse.statusCode)\n")
+                        completion([:])
+                    }
+                } else {
+//                    WriteToLog().message(stringOfText: "[Json.getRecord] error parsing JSON for \(existingDestUrl)\n")
+                    completion([:])
+                }   // if let httpResponse - end
+                semaphore.signal()
+                if error != nil {
+                }
+            })  // let task = destSession - end
+            //print("GET")
+            task.resume()
+        }   // getRecordQ - end
+    }
+    
+    func urlSession(_ session: URLSession, didReceive challenge: URLAuthenticationChallenge, completionHandler: @escaping(  URLSession.AuthChallengeDisposition, URLCredential?) -> Void) {
+        completionHandler(.useCredential, URLCredential(trust: challenge.protectionSpace.serverTrust!))
+    }
+    
+}
