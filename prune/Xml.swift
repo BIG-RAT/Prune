3d40b6a0cf1848d3b960406c0a0b9df9e96a2de6
leslie <leslie@ccs-indy.com>
2019-12-20 19:34:17 -0500

    with tableview - listing/removing works

--- prune/Xml.swift
+++ prune/Xml.swift
@@ -9,7 +9,7 @@
 import Cocoa
 
 class Xml: NSURL, URLSessionDelegate {
-    func action(action: String, theServer: String, base64Creds: String, theEndpoint: String, completion: @escaping (_ result: [String:AnyObject]) -> Void) {
+    func action(action: String, theServer: String, base64Creds: String, theEndpoint: String, completion: @escaping (_ result: (Int,String)) -> Void) {
 
         let getRecordQ = DispatchQueue(label: "com.jamf.getRecordQ", qos: DispatchQoS.background)
     
@@ -20,7 +20,7 @@ class Xml: NSURL, URLSessionDelegate {
         existingDestUrl = existingDestUrl.replacingOccurrences(of: "//JSSResource", with: "/JSSResource")
         
 //        if LogLevel.debug { WriteToLog().message(stringOfText: "[Json.getRecord] Looking up: \(existingDestUrl)\n") }
-//        print("existing endpoints URL: \(existingDestUrl)")
+        print("existing endpoints URL: \(existingDestUrl)")
         let destEncodedURL = NSURL(string: existingDestUrl)
         let jsonRequest    = NSMutableURLRequest(url: destEncodedURL! as URL)
         
@@ -37,22 +37,29 @@ class Xml: NSURL, URLSessionDelegate {
 //                    print("[Json.getRecord] httpResponse: \(String(describing: httpResponse))")
                     if httpResponse.statusCode >= 200 && httpResponse.statusCode <= 299 {
                         do {
-                            let json = try? JSONSerialization.jsonObject(with: data!, options: .allowFragments)
-                            if let endpointJSON = json as? [String:AnyObject] {
-//                                if LogLevel.debug { WriteToLog().message(stringOfText: "[Json.getRecord] \(endpointJSON)\n") }
-                                completion(endpointJSON)
-                            } else {
-//                                WriteToLog().message(stringOfText: "[Json.getRecord] error parsing JSON for \(existingDestUrl)\n")
-                                completion([:])
-                            }
+                            let returnedXML = String(data: data!, encoding: String.Encoding(rawValue: String.Encoding.utf8.rawValue))!
+//                            let json = try? JSONSerialization.jsonObject(with: data!, options: .allowFragments)
+//                            if let endpointJSON = json as? [String:AnyObject] {
+////                                if LogLevel.debug { WriteToLog().message(stringOfText: "[Json.getRecord] \(endpointJSON)\n") }
+//                                completion(returnedXML)
+//                            } else {
+////                                WriteToLog().message(stringOfText: "[Json.getRecord] error parsing JSON for \(existingDestUrl)\n")
+//                                completion("")
+//                            }
+                            completion((httpResponse.statusCode,returnedXML))
                         }
                     } else {
 //                        WriteToLog().message(stringOfText: "[Json.getRecord] error HTTP Status Code: \(httpResponse.statusCode)\n")
-                        completion([:])
+                        print("[Xml.action] error HTTP Status Code: \(httpResponse.statusCode)\n")
+                        if action != "DELETE" {
+                            completion((httpResponse.statusCode,""))
+                        } else {
+                            completion((httpResponse.statusCode,""))
+                        }
                     }
                 } else {
 //                    WriteToLog().message(stringOfText: "[Json.getRecord] error parsing JSON for \(existingDestUrl)\n")
-                    completion([:])
+                    completion((0,""))
                 }   // if let httpResponse - end
                 semaphore.signal()
                 if error != nil {
